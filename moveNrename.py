ee496ee3e6ab9e969a225b6e7b4f29664ca72d17
Harsger <herrmannmaximilian@web.de>
2022-04-11 15:47:59 +0200

    parameter-map, extended directory-logic

--- moveNrename.py
+++ moveNrename.py
@@ -1,97 +1,160 @@
 # -*- coding: utf-8 -*-
 
-import sys, getopt
-import os
-import time
-import math
-import csv
-import time
-from array import array
-import subprocess
+import sys, getopt, os , copy
 
 filelist = []
 
-def fillFileList( dirPath , inputfile , specific , exclude ):
-    directoryList=os.listdir(dirPath)
-    filelist[:]=[]
-    for filename in directoryList:
+def fillFileList( directory , start , end , specific , exclude ) :
+    global filelist
+    directoryList = os.listdir( directory )
+    for filename in directoryList :
         if( 
-            filename.find(str(inputfile)) != -1
-            and filename.find(str(specific)) != -1 
-            and filename.find(str(exclude)) == -1
-        ):
-            print( filename )
+            filename.startswith( str(start) )
+            and filename.endswith( str(end) )
+            and     str(specific) in filename 
+            and not str(exclude)  in filename
+        ) :
             filelist.append( filename )
 
-def main(argv):
+def main(argv) :
 
-    usage = 'moveNrename.py -i <inputfile> -d <datapath> -o <outputdir> -p <phrase> -r <replace> -s <specifier> -n <notuse>'
-
-    filename = ''
-    inputfile = ''
-    datapath = ''
-    outputdir = ''
-    phrase = 'neverUseThisPhrase'
-    replace = ''
-    specifier = ''
-    notuse = 'neverUseThisPhrase'
+    global filelist
     
-    try:
-        opts, args = getopt.getopt(argv,"hi:d:o:p:r:s:n:",
-            [
-                "inputfile=",
-                "datapath=",
-                "outputdir=",
-                "phrase=",
-                "replace=",
-                "specifier=",
-                "notuse="
-            ]
+    neverUse = 'neverUseThisPhrase'
+    
+    parameters = {                          
+        'filename'    : ''       ,
+        'ending'      : ''       ,
+        'inpath'      : ''       ,
+        'outpath'     : ''       ,
+        'phrase'      : neverUse ,
+        'replacement' : ''       ,
+        'specifier'   : ''       ,
+        'notuse'      : neverUse 
+    }
+    
+
+    shortParametersMap = {}
+    shortParametersString = 'h'
+    longParametersList = []
+    usage = 'python moveNrename.py'
+    
+    for p , v in parameters.items() :
+        shortParametersMap[str(p[0])] = str(p)
+        shortParametersString += str(p[0])+':'
+        longParametersList.append( str(p)+'=' )
+        usage += '\n \t\t -'+str(p[0])+' <'+str(p)+'>'
+    
+    try :
+        opts , args = getopt.getopt( 
+                                        argv , 
+                                        shortParametersString ,
+                                        longParametersList
+                                    )
+    except getopt.GetoptError :
+        print( str(usage) )
+        sys.exit(1)
+    
+    if len(opts) < 1 :
+        print( str(usage) )
+        sys.exit(2)
+    
+    for opt , arg in opts :
+        if opt in ("-h", "--help") :
+            print( str(usage) )
+            sys.exit(3)
+        shortOption = str( opt.replace( '-' , '' ) )[0]
+        if shortOption in shortParametersMap :
+            parameters[ shortParametersMap[shortOption] ] = arg
+           
+    if( parameters['inpath'] == '' ) :
+        print(' ERROR : \"inpath\" has to be specified ')
+        sys.exit(4)
+        
+    if not os.path.isdir( parameters['inpath'] ) :
+        print(' ERROR : \"inpath\" is no directory ')
+        sys.exit(5)
+        
+    if( 
+        parameters['outpath'] != '' 
+        and not os.path.isdir( parameters['outpath'] ) 
+    ) :
+        print(' ERROR : \"outpath\" is no directory ')
+        sys.exit(6)
+    
+    if( 
+        parameters['inpath'] == parameters['outpath' ]
+        and
+        (
+            parameters['phrase'] == neverUse
+            and
+            parameters['replacement'] == ''
         )
-    except getopt.GetoptError:
+    ) :
         print( str(usage) )
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt in ("-h", "--help"):
-            print usage
-            sys.exit()
-        elif opt in ("-i", "--inputfile"):
-            inputfile = arg
-        elif opt in ("-d", "--datapath"):
-            datapath = arg
-        elif opt in ("-o", "--outputdir"):
-            outputdir = arg
-        elif opt in ("-p", "--phrase"):
-            phrase = arg
-        elif opt in ("-r", "--replace"):
-            replace = arg
-        elif opt in ("-s", "--specifier"):
-            specifier = arg
-        elif opt in ("-n", "--notuse"):
-            notuse = arg
+        print(' ERROR : different paths and/or \n'+
+              '         phrase and replacement have to be specified' )
+        sys.exit(7)
     
-    if datapath == '':
+    fillFileList( 
+                    parameters['inpath'   ] , 
+                    parameters['filename' ] , 
+                    parameters['ending'   ] , 
+                    parameters['specifier'] , 
+                    parameters['notuse'   ] 
+                )
+    
+    if len(filelist) < 1 :
         print( str(usage) )
-        sys.exit(2)
+        sys.exit(8)
+    
+    for p , v in parameters.items() :
+        print( str(p)+'\t:\t'+str(v) )
 
-    if outputdir == '':
-        outputdir = datapath
-                    
-    print " inputfiles    : "+str(inputfile)
-    print " datapath      : "+str(datapath)
-    print " outputdir     : "+str(outputdir)
-    print " phrase        : "+str(phrase)
-    print " replace       : "+str(replace)
-    print " specifier     : "+str(specifier)
-    print " notuse        : "+str(notuse)
-
-    fillFileList( datapath , inputfile , specifier , notuse )
-
-    for filename in filelist:
-        outname = filename
-        outname = outname.replace(phrase,replace)
-        outname = outputdir+"/"+outname
-        command = "mv "+str(datapath)+"/"+str(filename)+" "+str(outname)
+    for filename in filelist :
+        if( 
+            parameters['phrase'] != neverUse
+            and
+            parameters['phrase'] not in filename 
+        ) :
+            continue
+        inname = copy.deepcopy( filename )
+        addition = ''
+        if( 
+            parameters['inpath'] != ''
+            and (
+                    parameters['inpath'] == '.'
+                    or 
+                    not parameters['inpath'].endswith('/')
+                )
+        ) :
+            addition = '/'
+        inname = (
+                    str(parameters['inpath'])
+                    +str(addition)+
+                    str(inname)
+                )
+        outname = copy.deepcopy( filename )
+        outname = outname.replace( 
+                                        parameters['phrase'     ] , 
+                                        parameters['replacement'] 
+                                    )
+        addition = ''
+        if( 
+            parameters['outpath'] != ''
+            and (
+                    parameters['outpath'] == '.'
+                    or 
+                    not parameters['outpath'].endswith('/')
+                )
+        ) :
+            addition = '/'
+        outname = (
+                    str(parameters['outpath'])
+                    +str(addition)+
+                    str(outname)
+                )
+        command = 'mv '+str(inname)+' '+str(outname)
         print( str(command) )
         os.system( str(command) )
 
