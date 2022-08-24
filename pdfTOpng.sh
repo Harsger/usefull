a003bd9a9946bb7bc5c2616e303a03c74a15c38b
Harsger <herrmannmaximilian@web.de>
2022-08-24 15:30:39 +0200

    other linux conversion tool

--- pdfTOpng.sh
+++ pdfTOpng.sh
@@ -1,4 +1,7 @@
 pdfname=$1
-pngname=${pdfname%.pdf}.png
 
-convert -density 150 -trim ${pdfname} -quality 100 ${pngname}
+pngname=${pdfname%.pdf}
+pdftoppm -png -cropbox -singlefile -r 300 ${pdfname} ${pngname}
+
+#pngname=${pdfname%.pdf}.png
+#convert -density 150 -trim ${pdfname} -quality 100 ${pngname}
